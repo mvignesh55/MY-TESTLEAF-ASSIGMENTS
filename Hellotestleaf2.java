f6520ed127e2977b99e8615a9ee4427e79233933
mvignesh55 <mvignesh55@gmail.com>
2024-12-08 00:08:22 +0530

    Add files via upload
    DAY 1 COMMIT CHANGES

---
+++ Hellotestleaf2.java
@@ -0,0 +1,33 @@
+package week1.day1;
+
+public class Hellotestleaf2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int xy=1500;
+		long phNum=9600326617l;
+		short value =12345;
+		float groupversion = 11.5f;
+		boolean black= false;
+		char key = 'B';
+		
+		
+		
+		System.out.println(xy);
+		System.out.println(phNum);
+		System.out.println(value);
+		System.out.println(groupversion);
+		System.out.println(black);
+		System.out.println("KEY");
+		
+		
+	
+		
+		
+		
+		
+	}
+
+}
---
+++ Hellotestleaf3.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Hellotestleaf3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int period=2005;
+		long phNum=9600326617l;
+		short value =12345;
+		float groupversion = 11.5f;
+		boolean black= false;
+		char key = 'B';
+				
+
+		System.out.println("period is: "+period);
+		System.out.println("phNum is:"+phNum);
+		System.out.println("value is :"+value);
+		System.out.println("groupversion is:"+groupversion);
+		System.out.println("blackcar is: "+black);
+		System.out.println("keylogo is:"+key);
+	}
+
+}
